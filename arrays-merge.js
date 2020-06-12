e186314b90830d185ab2dcd7d5a490111db3f907
Jennifer Jung <jenjung2194@gmail.com>
2020-06-12 22:16:17 +0000

    Create a function that merges two arrays and sort numbers by ascending order

---
+++ arrays-merge.js
@@ -0,0 +1,23 @@
+//merge two arrays together and sort ascending order
+
+const merge = function (arrayOne, arrayTwo) {
+  let newArray = arrayOne.concat(arrayTwo);
+
+  newArray.sort(function (a,b) {
+    if (a < b) {
+      return -1;
+    } else if (a > b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+  return newArray;
+}
+
+
+
+//test
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
